aaea857c4bd89233d73bb5337730b2dd43fbdb64
Lucas Dias Lopes <lucas.fulldev@gmail.com>
2020-04-06 21:23:13 -0300

    add feature de lightbox e selecao de imagens

--- galeria.js
+++ galeria.js
@@ -1,20 +1,28 @@
-let imagens= document.querySelectorAll('.small_img');
-let modal = document.querySelector('.modal');
-let modalImg = document.querySelector('#modal_img');
-let btClose = document.querySelector('#bt_close');
-let srcVal="";
+// seleciona apenas as imagens que serão usadas no lightbox
+const links = document.querySelectorAll('a[data-lightbox]');
+const modal = document.querySelector('.modal');
+const images = []; // array que recebe as imagens selecionadas
 
-for(let i =0; i<imagens.length;i++){
-    imagens[i].addEventListener('click',function(){
-        
-        srcVal = imagens[i].getAttribute('src');
-        modalImg.setAttribute('src', srcVal);
-        modal.classList.toggle('modal_active');
-    });
-    
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', selectImage);
 }
 
-
-btClose.addEventListener('click', function(){
+// close modal
+let btClose = document.querySelector('#bt_close');
+btClose.addEventListener('click', function () {
     modal.classList.toggle('modal_active');
 });
+
+/**
+ * Seleciona imagem
+ * Guarda a imagem no array e atualiza o modal
+ */
+function selectImage(event) {
+    event.preventDefault();
+    const link = event.target.parentNode;
+    const imagePlaceholder = document.querySelector('#modal_img');
+
+    modal.classList.toggle('modal_active');
+    imagePlaceholder.src = link.href;
+    images.push(link.href);
+}
